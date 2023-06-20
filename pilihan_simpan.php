a21a59983c0c14ac826dbba7fbf7b24fe18e8a9e
Shi Yang <shiyangsaw@gmail.com>
2023-06-20 22:40:46 +0800

    left testing n debug (see sz's tmr)

--- pilihan_simpan.php
+++ pilihan_simpan.php
@@ -1,16 +1,19 @@
 <?php
+// sambung ke p/data
 require 'connect.php';
 // terima nilai dari produk jumpa
 if(isset($_POST['submit'])){
     $produk = $_POST['idProduk'];
     $pengguna = $_POST['idPengguna'];
-
+    // masuk rekod dlm table
     $baharu = "INSERT INTO pilih VALUES (NULL,'$pengguna','$produk')";
+    // laksana arahan sql
     $pilihan = mysqli_query($conn, $baharu);
-
+    // mesej jika berjaya
     if ($pilihan) {
-        echo "<script> window.location:'produk_detail.php?
-        id=$produk'</script>";
+        echo "<script> window.location='produk_detail.php?id=$produk'</script>";
+    } else {
+        echo "<script>alert('Gagal direkodkan');window.location='dashboard.php'</script>";
     }
 }
 ?>
--- produk_edit.php
+++ produk_edit.php
@@ -1,26 +1,28 @@
 <?php
-include "header.php";
+include "header.php"; // panggil header
 
-$idProduk=$_GET['id'];
+$idProduk=$_GET['id']; // dapat url
 
 $dataProduk = mysqli_query($conn, "SELECT * FROM produk AS t1
-inner join jenama AS t2 on t1.idProduk=t2.idJenama 
-where t1.idProduk='$idProduk'");
+INNER JOIN jenama AS t2 ON t1.idProduk=t2.idJenama 
+WHERE t1.idProduk='$idProduk'");
+$qProduk=mysqli_fetch_array($dataProduk);
 ?>
 <html>
+<!-- panggil menu -->
 <div id="menu">
     <?php include 'menu2.php' ; ?>
 </div>
-
-<head?> 
+<!-- panggil isi -->
+<head> 
     <h2 style="text-align:center">EDIT PRODUK</h2>
 </head> 
 <body> 
     <form method="POST" action="produk_kemaskini.php" enctype="multipart/form-data">
-    <p>Nama Produk<br>
+    <p>NAMA PRODUK<br>
     <input type="text" name="nama" value="<?php echo $qProduk['namaProduk']; ?>" 
     size="50" required autofocus></p>
-    <p>Jenama<br>
+    <p>JENAMA<br>
     <select name="jenama">
     <?php
     echo "<option selected value='$qProduk[idJenama]'> $qProduk[namaJenama]</option>";
@@ -32,17 +34,17 @@ where t1.idProduk='$idProduk'");
     ?>
     </select>
     </p>
-    <p>Harga<br>
+    <p>HARGA<br>
     <input type="text" name="harga" value="<?php echo $qProduk['harga']; ?>" 
     size="10" required></p> 
-    <p>Detail Produk (Deskirpsi)<br>
+    <p>DETAIL PRODUK<br>
     <textarea name="detail" rows="10" cols="50" required>
         <?php echo $qProduk['detail']; ?></textarea></p>
-    <p>Gambar <br>
+    <p>GAMBAR<br>
     <img src="gambar/<?php echo $qProduk['gambar']; ?>"
     width="30%" height="auto">
-    <input type="text" name="id" value="<?php echo $qProduk['idProduk']; ?>" hidden> <br>
-
+    <input type="text" name="id" value="<?php echo $qProduk['idProduk']; ?>" hidden>
+    <br>
     <button name="submit" type="submit">SIMPAN</button> <br> 
     <font color= 'red'>Pastikan Maklumat yang dimasukkan adalah betul</font> 
     </p> 
