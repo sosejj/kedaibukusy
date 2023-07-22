9a130b83a06a6bc563eff956af7593f287cbb696
Shi Yang <shiyangsaw@gmail.com>
2023-07-22 23:20:36 +0800

    changed password attribute

--- signup.php
+++ signup.php
@@ -12,7 +12,7 @@
                     minlength = "12" maxlength = "12" size = "30" required autofocus>
                 </p>
                 <p>Kata Laluan: <br> <!-- input password -->
-                    <input type = "text" name = "password" placeholder = "5 hingga 20 huruf/nombor"
+                    <input type = "password" name = "password" placeholder = "5 hingga 20 huruf/nombor"
                     minlength = "5" maxlength = "20" size = "30" required>
                 </p>
                 <p>Nama Pengguna: <br> <!-- input nama -->
