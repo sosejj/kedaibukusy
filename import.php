642777f941a3bdab9b24750d1f831e4c025f0e2b
Shi Yang <shiyangsaw@gmail.com>
2023-07-22 23:52:45 +0800

    tidied import.php

--- import.php
+++ import.php
@@ -4,25 +4,23 @@ include 'header.php';
 <html>
     <body>
         <div id="menu">
-        <?php include 'menu2.php'; ?></div>
-
+            <?php include 'menu2.php'; ?>
+        </div>
         <div id="isi">
-            <center><h2>IMPORT JENAMA DARI FAIL CSV</h2></center>
-            <label> Pilih lokasi fail CSV/Excel</label>
+            <h2><u>Import Jenama daripada Fail CSV</u></h2>
+            <label>Pilih lokasi fail .csv</label><br>
 
             <!-- upload fail CSV -->
-            <form action="import_simpan.php" method="POST"
-             enctype="multipart/form-data">
-                <input type="file" name="file" accept=".csv"><br>
-                <button type="submit" id="submit" name="import">
-                  UPLOAD</button>
+            <form action="import_simpan.php" method="POST" enctype="multipart/form-data">
+                <input type="file" name="file" accept=".csv"><br><br>
+                <button type="submit" id="submit" name="import">UPLOAD</button>
             </form>
             *Contoh: <br>
-            JENAMA1 <br>
-            JENAMA2 <br>
-            JENAMA3 <br>
-            JENAMA4
-            <p>Fail mesti dalam bentuk .csv</p>
+            JENAMA1, <br>
+            JENAMA2, <br>
+            JENAMA3, <br>
+            JENAMA4,
+            <p style="color: red;">* Fail mesti dalam bentuk .csv</p>
 </div>
 </body>
 </html>
